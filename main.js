eb4a8905e6dbc94725a4321e680b1942f624d56b
Filip Ruminski <fruminski@gmail.com>
2023-03-01 12:37:09 +0000

    added functionality and css

--- main.js
+++ main.js
@@ -1,30 +1,71 @@
-const movieCard = document.querySelector(".movie-card");
-const title = document.querySelector(".title");
-const poster = document.querySelector(".poster");
-const year = document.querySelector(".year");
+const searchButton = document.querySelector("button");
+const input = document.querySelector(".input");
+const display = document.querySelector(".display-area-container");
+const message = document.querySelector(".message");
 
 async function getMovies() {
   try {
+    if (!input.value) {
+      alert("Please enter a movie name");
+    }
     let response = await fetch(
-      `http://www.omdbapi.com/?apikey=6aaaa222&s=rambo`
+      `http://www.omdbapi.com/?apikey=6aaaa222&s=${input.value}`
     );
     let data = await response.json();
-    console.log(data);
+    if (data.Response === "False") {
+      message.textContent = data.Error;
+    }
 
-    data.Search.map((movie) => {
-      title.textContent = movie.Title;
-      poster.src = movie.Poster;
-      year.textContent = movie.Year;
-    });
+    console.log(data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-getMovies();
-// movieCard = {
-//   title: data.Search.Title,
-//   poster: data.Search.Poster,
-//   genre: data.Search.Genre,
-//   rating: data.Search.imdbRating
-// };
+const createMovieCard = (movie) => {
+  const { Title, Poster, Year, Type } = movie;
+
+  console.log(Title, Poster, Year, Type);
+  const card = document.createElement("div");
+  card.classList.add("movie-card");
+
+  const imgElem = document.createElement("img");
+  imgElem.src = Poster;
+  imgElem.alt = " movie poster";
+  card.appendChild(imgElem);
+
+  const titleElem = document.createElement("h2");
+  titleElem.textContent = Title;
+  titleElem.classList.add("title");
+  card.appendChild(titleElem);
+
+  const typeElem = document.createElement("p");
+  typeElem.textContent = Type;
+  card.appendChild(typeElem);
+
+  const yearElem = document.createElement("p");
+  yearElem.textContent = Year;
+  card.appendChild(yearElem);
+
+  return card;
+};
+
+const displayMovies = async () => {
+  const payload = await getMovies();
+  if (payload.Response === "False") {
+    display.textContent = payload.Error;
+  } else {
+    display.textContent = "";
+  }
+
+  const fragment = document.createDocumentFragment();
+  payload.Search.forEach((movie) => {
+    const card = createMovieCard(movie);
+    fragment.appendChild(card);
+  });
+
+  display.appendChild(fragment);
+};
+
+searchButton.addEventListener("click", displayMovies);
